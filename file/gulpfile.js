31e9f44edde2f7d451e0fdcca1d0c8740504ca22
huina.gu <huina.gu@qitmac000263deMacBook-Pro.local>
2015-06-08 15:45:54 +0800

    file

---
+++ file/gulpfile.js
@@ -0,0 +1,30 @@
+// 引入 gulp
+var gulp = require('gulp');
+
+// 引入组件
+var compass = require('gulp-compass');
+
+// style path
+var scssPath = './usage/page';
+var cssPath = './usage/export';
+
+// 编译Sass
+gulp.task('compass', function() {
+  gulp.src(scssPath + '/*.scss')
+    .pipe(compass({
+        config_file: './config.rb',
+        css: cssPath,
+        sass: scssPath,
+        style: 'expanded'
+    }))
+    // 本行不能删除，否则只能编译不超过16个文件
+    .pipe(gulp.dest(cssPath));
+});
+
+// watch变更
+gulp.task('watch', function() {
+    gulp.watch('./**/*.scss', ['compass']);
+});
+
+// 默认任务
+gulp.task('default', ['compass', 'watch']);
